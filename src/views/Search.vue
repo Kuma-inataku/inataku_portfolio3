a19006cf28ae54a21d9335d3d1260bbbafd40f00
kuma <inataku0121@gmail,com>
2021-01-02 13:15:52 +0900

    loading機能追加

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,7 +1,232 @@
-
 <template>
   <div>
-    <h1>Vuetify for search</h1>
-    <img src="..\assets\images\detail_1.jpg" alt="たび">
+      <div v-show="loading">
+    <div class="fullview">
+      <div class="loading-spacer"></div>
+      <vue-loading 
+        type="spiningDubbles"
+        color="#64B5F6"
+        :size="{ width: '100px', height: '100px' }"
+        >
+      </vue-loading>
+      <Loading v-show="loading"></Loading>
+    </div>
+  </div>
+
+    <div class="container">
+      <div class="l-search">
+        <div class="l-main">
+          <div class="container">
+        <h3>フリーワード検索</h3>
+      <div class="t-c-search">
+        <form action="">
+          <input type="text" class="t-word-search" placeholder="" name="freeword">
+        </form>
+        <button type="submit" class="t-word-submit">検索</button>
+      </div>
+      <!-- <router-link to="/page3" class="router">DEATAIL</router-link> -->
+      <div class="t-other_search">
+      </div>
+    </div>
+          <div class="l-result">  
+            <h2>検索結果</h2>
+            <!-- <p>「○○」の検索結果：<span class="l-result-num">5</span>件</p> -->
+            <!-- <div class="l-result-sub"> -->
+              <!-- <p><span class="l-result-num">5</span>件の内、<span class="l-result-num-1st">1</span>～<span class="l-result-num-1st">5</span>件を表示</p> -->
+            <!-- </div> -->
+          </div>
+            <div class="l-list">
+              <ul>
+                <li>
+                  <a href="#">
+                    <div class="l-content">
+                      <div class="l-img">
+                        <img src="static/images/top_image1.jpg" alt="">
+                      </div>
+                      <div class="l-detail">
+                        <h3 class="l-title">テスト</h3>
+                        <p>テストテストテストテストテストテストテストテストテストテストテスト</p>
+                        <p class="l-fav">"○○"いいね！</p>
+                        <!-- <img src="../images/good1.png" alt=""> -->
+                      </div>
+                    </div>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <div class="l-content">
+                      <div class="l-img">
+                        <!-- <img src="../images/top_image1.jpg" alt=""> -->
+                      </div>
+                      <div class="l-detail">
+                        <h3 class="l-title">テスト</h3>
+                        <p>テストテストテストテストテストテストテストテストテストテストテスト</p>
+                        <p class="l-fav">"○○"いいね！</p>
+                        <!-- <img src="../images/good1.png" alt=""> -->
+                      </div>
+                    </div>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <div class="l-content">
+                      <div class="l-img">
+                        <!-- <img src="../images/top_image1.jpg" alt=""> -->
+                      </div>
+                      <div class="l-detail">
+                        <h3 class="l-title">テスト</h3>
+                        <p>テストテストテストテストテストテストテストテストテストテストテスト</p>
+                        <p class="l-fav">"○○"いいね！</p>
+                        <!-- <img src="../images/good1.png" alt=""> -->
+                      </div>
+                    </div>
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <p class="l-page">前 | 1 | 2 | 3 | 4 | 5 | 次</p>
+        </div>
+        <div class="l-side">
+          <div class="category">
+            <h2>カテゴリ検索</h2>
+            <ul class="t-m-category">
+              <li>
+                <a href="">フェス 初心者</a>
+              </li>
+              <li>
+                <a href="">ライブハウス 初心者</a>
+              </li>
+              <li>
+                <a href="">フェス 注意点</a>
+              </li>
+              <li>
+                <a href="">ツーステ</a>
+              </li>
+              <li>
+                <a href="">ROCKIN'ON JAPAN</a>
+              </li>
+              <li>
+                <a href="">日本の米は世界一</a>
+              </li>
+            </ul>
+          </div>
+          <div class="t-side">
+          <h3>「めっちゃE」ランキング検索</h3>
+          <ul class="t-s-search">
+            <li class="ranking_1">
+              <a href="#">【失敗しない！】初心者必見！フェスに持っていくもの10選！
+              </a>
+            </li>
+            <li class="ranking_2">
+              <a href="#"> 【これだけ気を付けよう！】フェスを楽しむための注意点3選！</a>
+            </li>
+            <li class="ranking_3">
+              <a href="#">【完全攻略！】初心者邦ロックロードマップ！</a>
+            </li>
+          </ul>
+        </div>
+
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+import Loading from '@/components/Loading'
+
+export default {
+  data(){
+    return{
+      loading:true
+    }
+  },
+    mounted() {
+    setTimeout(() => {
+      this.loading = false;
+    }, 1000);
+  },
+  components: {
+    Loading,
+    // Home,
+  },
+};
+
+</script>
+
+<style>
+/* ここからリストページ */
+.container{
+  color: #333;
+}
+.l-search{
+  display: flex;
+  flex-flow: row;
+  padding-top: 20px;
+}
+.l-main{
+  width: 70%;
+  margin: 0 80px 0 30px;
+}
+.l-result span{
+  font-weight: bold;
+}
+.l-result-sub{
+  display: flex;
+  justify-content: space-between;
+}
+.l-list li{
+  padding: 10px 0;
+  border-bottom: 3px dotted #ccc;
+  background: #fff;
+  margin: 10px 0;
+}
+.l-content{
+  display: flex;
+}
+.l-img img{
+  width: 200px;
+}
+.l-detail{
+  margin-left: 20px;
+}
+.l-title{
+  color: #0066CC;
+}
+.l-fav{
+  background: beige;
+  display: inline-block;
+}
+.l-page{
+  margin: 20px 0;
+  text-align: center;
+}
+/* ここからサイドバー */
+.l-side{
+  width: 30%;
+  margin: 0 30px 0 0;
+}
+/* ここからフリーワード検索 */
+.l-c-search{
+  border-bottom: 2px dotted #ccc;
+  background: #fff;
+}
+h2{
+  margin-bottom: 30px;
+}
+/* ここからカテゴリ検索 */
+.category{
+  border-bottom: 2px dotted #ccc;
+  background: #fff;
+  margin-top: 20px;
+}
+.t-m-category{
+  display: flex;
+  flex-wrap: wrap;
+}
+.t-m-category li{
+  background:rgb(230, 230, 230, 0.8);
+  margin: 10px;
+  padding: 10px;
+}
+</style>
